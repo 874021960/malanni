346d8344f4a6a731ed03cba26e035c16b146ccfa
yangshuwen <874021960@qq.com>
2022-03-10 11:48:17 +0800

    api文件夹创建

---
+++ malanni/src/api/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Description: 页面描述
+ * @Author: yangshuwen
+ * @Date: 2022-03-10 11:46:15
+ * @LastEditors: yanshuwen
+ * @astEditTime: Do not edit
+ */
+export default {
+    name:"yang"
+}
--- malanni/src/main.js
+++ malanni/src/main.js
@@ -1,7 +1,15 @@
+/*
+ * @Description: 页面描述
+ * @Author: yangshuwen
+ * @Date: 2022-03-10 11:28:32
+ * @LastEditors: yanshuwen
+ * @astEditTime: Do not edit
+ */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import xinxi from "./api/index.js"
+console.log(xinxi.name)
 Vue.config.productionTip = false
 
 new Vue({
